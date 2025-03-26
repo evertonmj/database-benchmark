22689243d4b2252b584d346fb3edf8ef6989cc02
Everton Mendonca Jesus <evertonmj@gmail.com>
2025-03-25 22:30:59 -0300

    Feat: added memcache

--- src/main/java/utils/annotations/ConfigProperty.java
+++
@@ -1,9 +0,0 @@
-package utils.annotations;
-
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-
-@Retention(RetentionPolicy.RUNTIME)
-public @interface ConfigProperty {
-    String value();
-}
